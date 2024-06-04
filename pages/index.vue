8661fdd99135c301d3989d94c751b46c8280feb3
blue <dancingblue2009@gmail.com>
2024-06-04 12:44:51 +0700

    index page

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,13 @@
   <CommonHeader1 :title="$t('home.header.title')" :content="$t('home.header.content')" :btn="$t('home.header.btn')"
     href="#" :hint="$t('home.header.hint')" pic="/images/home/home_banner_graph.png" pic-width="424px"
     pic-height="466px" />
-  <!-- <HomeList1 />
+  <HomeList1 />
   <HomeList2 />
   <HomeJoin1 />
   <HomeJoin2 />
   <CommonSuperCharge :title="$t('home.superCharge.title')" :sub-title="$t('home.superCharge.subTitle')"
     :tabs="$tm('home.superCharge.tabs')" />
-  <Footer /> -->
+  <Footer />
 
   <Notification />
 </template>
