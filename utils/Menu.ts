13bf43e76600c0fc5e439baa307ce257f93d15f6
lcf <chuanfa_li@163.com>
2024-07-25 20:22:19 +0800

    feat：菜单数据

---
+++ utils/Menu.ts
@@ -0,0 +1,130 @@
+export const menu = [
+  {
+    "name": "Network",
+    "children": [
+      {
+        "name": "Ddos",
+        "children": [
+          { "name": "Overview" },
+          { "name": "SmartEdge" },
+          {
+            "name": "Tools",
+            "children": [
+              { "name": "DNS Lookup" },
+              { "name": "HTTP Test" },
+              { "name": "Latency Test" },
+              { "name": "Traceroute" },
+              { "name": "Diagnostic Report" },
+              { "name": "Other Tools" }
+            ]
+          },
+          { "name": "Affiliate Program" },
+          { "name": "Pricing" }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Products",
+    "children": [
+      {
+        "name": "CDN",
+        "children": [
+          { "name": "Overview" },
+          { "name": "Features" },
+          {
+            "name": "Features",
+            "children": [
+              { "name": "PermaCache" },
+              { "name": "SafeHop" }
+            ]
+          },
+          {
+            "name": "Comparisons",
+            "children": [
+              { "name": "AWS" },
+              { "name": "Fastly" },
+              { "name": "CDN77" },
+              { "name": "CloudFlare" },
+              { "name": "Akamai" }
+            ]
+          },
+          {
+            "name": "Storage",
+            "children": [
+              { "name": "Africa/Middle East" },
+              { "name": "Asia" },
+              { "name": "Australia" },
+              { "name": "Europe" },
+              { "name": "Latin America" },
+              { "name": "North America" },
+              { "name": "Bouuy-storage" },
+              { "name": "SSD-storage" }
+            ]
+          }
+        ]
+      },
+      {
+        "name": "Stream",
+        "children": [
+          { "name": "MediaCage" },
+          { "name": "MultiDRM" },
+          { "name": "Overview" },
+          { "name": "Player" },
+          { "name": "Transcribe_Al" }
+        ]
+      },
+      {
+        "name": "DNS",
+        "children": [
+          { "name": "Bouuy-DNS" },
+        ]
+      }
+    ]
+  },
+  {
+    "name": "resources",
+    "children": [
+      { "name": "about" },
+      { "name": "careers" },
+      { "name": "Contact" },
+      { "name": "story" },
+      { "name": "Super Polyfill" },
+      { "name": "blog" },
+      { "name": "CDN-What is CDN" },
+      {
+        "name": "caseStudiesDetails",
+        "children": [
+          { "name": "Eskimi" },
+          { "name": "InnoGames" },
+          { "name": "Nexusmods" },
+          { "name": "Nzxt" },
+          { "name": "OTT" },
+          { "name": "ParrotSec" },
+          { "name": "Timbo" },
+          { "name": "Valnet" },
+          { "name": "Younity" }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Solutions",
+    "children": [
+      { "name": "SecurityCDN" },
+      { "name": "StorageGame" },
+      { "name": "StorageSoftware" },
+      { "name": "VideoDelivery" },
+      { "name": "WebsiteAcceleration" },
+      { "name": "WebsiteWordpress" }
+    ]
+  },
+  { "name": "BookMeeting" },
+  { "name": "index 首页" },
+  { "name": "Log In" },
+  { "name": "Sign Up" },
+  { "name": "ResetPassword" },
+  { "name": "Contact Sales" },
+  { "name": "SLA" },
+  { "name": "Support HUB" }
+]
