183c4ef5ade2e7f4364cabf9c3c5b921142acaef
yutaka <yutaka@inc-acb.com>
2025-04-02 08:01:02 -0300

    fix: fixed ui issues

--- components/Common/SuperCharge.vue
+++ components/Common/SuperCharge.vue
@@ -14,7 +14,7 @@ const curTab = ref(0);
 
 watch(curTab, (newCurTab) => {
     gsap.fromTo(
-      ".tabs-content",
+      ".left-image",
       { y: 20, opacity: 0 },
       { y: 0, opacity: 1, duration: 0.5, delay: 0.6 }
     );
